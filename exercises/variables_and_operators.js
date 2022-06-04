933e90420cce9687b920f37981931ed52ea5283b
KiaraMPais <kikipais@hotmail.com>
2022-06-04 18:42:33 -0300

    Initial Commit

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,17 @@
+// Ejercicio 1.A
+const primerNumero = 30;
+const segundoNumero = 220;
+const tercerNumero = primerNumero + segundoNumero;
+console.log(tercerNumero);
+
+// Ejercicio 1.B
+const primeraString = 'Concatenando';
+const segundaString = 'Strings';
+const terceraString = firstString + secondString;
+console.log(terceraString);
+
+// Ejercicio 1.C
+const primeraStringC = 'Concatenando';
+const segundaStringC = 'Strings';
+const resultado = primeraStringC.length + segundaStringC.length;
+console.log(resultado);
